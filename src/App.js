0263ca7b6f31137314d087c7930d81015ebfa45a
pascalvdbosch <pascalvdbosch@hotmail.com>
2023-05-24 11:13:15 +0200

    Mensen

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+    Er wordt gewerkt aan deze website. 
+<br/>
+    De volgende mensen werken bij het TLC Delft Leiden: 
+<br/>
+    <ul>
+      <li>Hilde Wijngaard</li>
+      <li>Boaz van der Plas</li>
+      <li>Jeroen Oostinga</li>
+      <li>Gunter Helms</li>
+      <li>Ruud Vermeij</li>
+      <li>Peter Denteneer</li>
+      <li>Marijn Versteegh</li>
+      <li>Pascal van den Bosch</li>
+      <li>Lodewijk Hendrik Arntzen</li>
+    </ul>
+  </div>;
 }
 
 export default App;
